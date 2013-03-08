86dec8f5b4ad41a223b2312fdc851ba271e4ae80
Chloe <chloe.stinetorf@gmail.com>
2013-03-08 10:18:00 -0500

    deleted tradr

--- r2013-03-08-tradr/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-R20130308Tradr::Application.config.secret_token = '080d66e70a8d732e3bccfd4d0521386198bb92e2b58396510dac0fb34b80899de75b1e427d875d18de00df3cdaa72027da901226f57c960a9cea81e1a9add708'
